196b917bdea1ab6ab8fa8633f2faba04b0629904
suraj sharma <surajneha2510@gmail.com@yourdomain.com>
2022-05-13 12:56:39 +1000

    change code javascript

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 console.log('Hello world')
-document.querySelector('.cross').getElementsByClassName.display = 'none';
-document.querySelector('.hamburger').addEventListener('click', () => {
-document.querySelector('.sidebar').classList.toggle('.sidebarGo');
+// document.querySelector('.cross').getElementsByClassName.display = 'none';
+// document.querySelector('.hamburger').addEventListener('click', () => {
+// document.querySelector('.sidebar').classList.toggle('.sidebarGo');
 
-});
+// });

7a2174996057e491c35ebcec4d117d4b0636b1ef
suraj sharma <surajneha2510@gmail.com@yourdomain.com>
2022-05-14 12:24:53 +1000

    javascript for hambug and cross sidebar

--- main.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello world')
-document.querySelector('.cross').getElementsByClassName.display = 'none';
-document.querySelector('.hamburger').addEventListener('click', () => {
-document.querySelector('.sidebar').classList.toggle('.sidebarGo');
-
-});
